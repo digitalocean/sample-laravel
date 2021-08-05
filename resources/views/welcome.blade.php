24cb9e679a21d4f49de54de99a843de588f464b1
gersonborges <passupreto@gmail.com>
2021-08-05 11:54:47 -0300

    'first'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Gasway</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,18 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Gasway
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://gasway.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://gasway-news.com">News</a>
+                    <a href="https://blog.gasway.com">Blog</a>
+                    <a href="https://nova.gasway.com">Nova</a>
+                    <a href="https://forge.gasway.com">Forge</a>
+                    <a href="https://vapor.gasway.com">Vapor</a>
+                    <a href="https://github.com/gasway/gasway">GitHub</a>
                 </div>
             </div>
         </div>
