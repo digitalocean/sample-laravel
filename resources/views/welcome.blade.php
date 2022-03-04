d06ea3f89c04c2f85871f19ce105aeef1df2c07f
sirelkhatim Gamal <32894519+seralkhatem@users.noreply.github.com>
2022-03-04 15:40:30 +0300

    100001

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NBC
                 </div>
 
                 <div class="links">
