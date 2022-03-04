8bb7c6ec9ea2643f7940891953785d337050e8de
sirelkhatim Gamal <32894519+seralkhatem@users.noreply.github.com>
2022-03-04 21:42:06 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    NBCNBC
+                    NBCNBC https://github.com/digitalocean/sample-laravel.git
                 </div>
 
                 <div class="links">
