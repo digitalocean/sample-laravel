dfe62b3555af7321b7498b9cce4b32320f585e5f
sirelkhatim Gamal <32894519+seralkhatem@users.noreply.github.com>
2022-03-04 15:25:53 +0300

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NBC new TEST
                 </div>
 
                 <div class="links">
