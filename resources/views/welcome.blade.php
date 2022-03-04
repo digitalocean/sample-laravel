9701f2211a7e5da307be3778a445a456b45b8e13
sirelkhatim Gamal <32894519+seralkhatem@users.noreply.github.com>
2022-03-04 21:35:48 +0300

    NBC

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    NBC
+                    NBCNBC
                 </div>
 
                 <div class="links">
