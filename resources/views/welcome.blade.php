20a680e2390d489065d95b5ddb38d406bf327d71
sirelkhatim Gamal <32894519+seralkhatem@users.noreply.github.com>
2022-03-05 19:57:42 +0300

    user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                 NBCNBC https://github.com/digitalocean/sample-laravel.git w
                 NBCNBC https://github.com/digitalocean/sample-laravel.git w
-                     <h2>{{$user->name}}</h2>
+                     <h2>{ {$ user->name}}</h2>
                  </div>
                  NBCNBCNBCNBC a  https://github.com/digitalocean/sample-laravel.git w
                  Welcome Welcome 
