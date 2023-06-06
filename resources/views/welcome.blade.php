cf76d25bbc2bbf7a0f4af7e4936fc153e287f146
kickx182 <54664860+kickx182@users.noreply.github.com>
2023-06-06 12:52:27 +0200

    more tests
    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel KT
+                    Laravel KTv2
                 </div>
 
                 <div class="links">
