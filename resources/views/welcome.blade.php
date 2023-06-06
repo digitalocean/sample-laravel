1ee546634137f7274f74b6c53d7c382e786742b1
kickx182 <54664860+kickx182@users.noreply.github.com>
2023-06-06 12:41:16 +0200

    test
    my name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel KT
                 </div>
 
                 <div class="links">
