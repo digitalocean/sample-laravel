8b26ec0e66e4f0dfa9a05a4be26ca889b750804b
Trajkov <54664860+kickx182@users.noreply.github.com>
2023-06-14 10:17:41 +0200

    test
    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel KTv2
+                    Laravel KTv3
                 </div>
 
                 <div class="links">
